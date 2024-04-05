08ee94ae2887bbb9a70677f70244c0ea35519fcf
behzuzzbek <behruztillayev0724@gmail.com>
2024-04-05 02:11:05 -1200

    edu-free

---
+++ src/components/sections/index.tsx
@@ -0,0 +1,5 @@
+import Intro from "./intro";
+import Contact from "./contact";
+
+
+export{ Intro , Contact } 
---
+++ src/types/interface/index.ts
@@ -0,0 +1,41 @@
+// --------- Authorization  -------------
+
+export interface Signin{
+    username: string;
+    password: string|number;
+}
+
+export interface Signup extends Signin{
+    name: string;
+    phone: string;
+}
+
+export interface ResetPassword{
+    email?: string;
+    phone?: string|number;
+}
+
+
+export interface Request{
+    signin:(data:Signin)=>any,
+    signup:(data:Signup)=>any,
+    signout:()=>void,
+    reset:(data:ResetPassword)=>void
+}
+
+
+
+
+// ------------------------------------
+
+
+
+
+
+
+
+// --------------------- REACT TSX , JSX  Elements --------------------------------
+
+export interface propTypes {
+    children:string | any | null
+}
